86f518cad17c92b44f62110142ac456c5ff76b85
rh-cpranava <81606179+rh-cpranava@users.noreply.github.com>
2021-10-14 12:50:35 +0800

    Update release-upload-mirror.sh

--- release-upload-mirror.sh
+++ release-upload-mirror.sh
@@ -8,19 +8,13 @@ ARCHITECTURE=x86_64
 RELEASE_NAME="ocp-release" 
 
 echo "Sourcing ImageContentSourcePolicy for this release"
-
 oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} --run-dry
 
 echo "Downloading oc client for OCP ${OCP_RELEASE}"
-
-wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-client-linux.tar.gz" -O "/root/mirror/openshift-client-linux-${OCP_RELEASE}.tar.gz"
-
-echo "Downloading openshift-install client for OCP ${OCP_RELEASE}"
-
-wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-install-linux.tar.gz" -O "/root/mirror/openshift-client-linux-${OCP_RELEASE}.tar.gz"
+wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-client-linux.tar.gz" -O "$REMOVABLE_MEDIA_PATH/oc-${OCP_RELEASE}.tar.gz"
 
 echo "Downloading openshift-install client for OCP ${OCP_RELEASE}"
+wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-install-linux.tar.gz" -O "$REMOVABLE_MEDIA_PATH/openshift-install-${OCP_RELEASE}.tar.gz"
 
 echo "Push the release images to the local registry for OCP ${OCP_RELEASE}"
-
-oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 
+oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} --apply-release-image-signature

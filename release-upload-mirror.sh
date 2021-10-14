c77b079cb90eded7e4a1beec6452c847ec879f67
rh-cpranava <81606179+rh-cpranava@users.noreply.github.com>
2021-10-14 12:17:50 +0800

    Update and rename release-mirror.sh to release-upload-mirror.sh

--- release-upload-mirror.sh
+++ release-upload-mirror.sh
@@ -1,20 +1,14 @@
+OCP_RELEASE=$1
 LOCAL_REGISTRY=localhost:5000
 LOCAL_REPOSITORY=ocp-release/openshift4
 PRODUCT_REPO='openshift-release-dev'
 LOCAL_SECRET_JSON=/root/pull.json
 REMOVABLE_MEDIA_PATH=<tmp-folder>
+ARCHITECTURE=x86_64
 
 echo "Sourcing ImageContentSourcePolicy for this release"
 
 oc adm release mirror -a ${LOCAL_SECRET_JSON} --to-dir=${REMOVABLE_MEDIA_PATH}/mirror quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --dry-run
-
-echo "Mirroring content for OCP ${OCP_RELEASE}"
-
-oc adm release mirror -a ${LOCAL_SECRET_JSON} --to-dir=${REMOVABLE_MEDIA_PATH}/mirror quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE}
-
-echo "Downloading oc client for OCP ${OCP_RELEASE}"
-
-wget "https://mirror.openshift.com/pub/openshift-v4/clients/oc/$OCP_RELEASE/linux/oc.tar.gz" -O "/root/mirror/openshift-client-linux-${OCP_RELEASE}.tar.gz"
 
 echo "Downloading oc client for OCP ${OCP_RELEASE}"
 
@@ -23,3 +17,9 @@ wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/ope
 echo "Downloading openshift-install client for OCP ${OCP_RELEASE}"
 
 wget "https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-install-linux.tar.gz" -O "/root/mirror/openshift-client-linux-${OCP_RELEASE}.tar.gz"
+
+echo "Downloading openshift-install client for OCP ${OCP_RELEASE}"
+
+echo "Push the release images to the local registry for OCP ${OCP_RELEASE}"
+
+oc adm -a ${LOCAL_SECRET_JSON} release mirror --from=quay.io/${PRODUCT_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${ARCHITECTURE} --to=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY} --to-release-image=${LOCAL_REGISTRY}/${LOCAL_REPOSITORY}:${OCP_RELEASE}-${ARCHITECTURE} 

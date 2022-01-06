723d58033c406393454db31662523fcf223e0fad
rh-cpranava <81606179+rh-cpranava@users.noreply.github.com>
2022-01-06 15:59:39 +0800

    Create create-mirror-registry.sh

---
+++ create-mirror-registry.sh
@@ -0,0 +1,2 @@
+
+podman run --name mirror-registry -p 5000:5000 -v /opt/registry/data:/var/lib/registry:z -v /opt/registry/auth:/auth:z -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /opt/registry/certs:/certs:z -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/Ext-Registry-CA.pem -e REGISTRY_HTTP_TLS_KEY=/certs/Ext-Registry-CA.key -e REGISTRY_COMPATIBILITY_SCHEMA1_ENABLED=true -d docker.io/library/registry:2.7.1

042fcea2f58fb38ac4cc8e1902ec81e72dad4277
rh-cpranava <81606179+rh-cpranava@users.noreply.github.com>
2022-01-13 13:59:22 +0800

    Update oc-verification.sh

--- ocp-disconnected-upgrade/oc-verification.sh
+++ ocp-disconnected-upgrade/oc-verification.sh
@@ -1,5 +1,49 @@
-oc get clusterversion >> verification_${date +"%m-%d-%y-%T"}
-oc get clusteroperators >> verification_${date +"%m-%d-%y-%T"}
-oc get nodes -o wide >> verification_${date +"%m-%d-%y-%T"}
-oc get pods -A | grep -v "Running\|Completed\|Terminated|\Succeeded" >> verification_${date +"%m-%d-%y-%T"}
-oc get mcp -o yaml | grep -i pause >> verification_${date +"%m-%d-%y-%T"}
+filename=verification_$(date +"%m-%d-%y-%T")
+
+echo "oc get clusterversion"  >> $filename
+
+oc get clusterversion >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get clusteroperators"  >> $filename
+
+oc get clusteroperators >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get clusterversion"  >> $filename
+
+oc get clusterversion >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get nodes -o wide"  >> $filename
+
+oc get nodes -o wide >> $filename
+
+echo "***********************************" >> $filename
+
+echo 'oc get pods -A | grep -v "Running\|Completed\|Terminated|\Succeeded"'  >> $filename
+
+oc get pods -A | grep -v "Running\|Completed\|Terminated|\Succeeded" >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get mcp -o yaml | grep -i pause"  >> $filename
+
+oc get mcp -o yaml | grep -i pause >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get pod -o wide -n openshift-kube-apiserver"   >> $filename
+
+oc get pod -o wide -n openshift-kube-apiserver  >> $filename
+
+echo "***********************************" >> $filename
+
+echo "oc get pod -o wide -n openshift-etcd"  >> $filename
+
+oc get pod -o wide -n openshift-etcd  >> $filename
+
+echo "***********************************" >> $filename

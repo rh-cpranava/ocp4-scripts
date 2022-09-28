df8f4b2da7856800140e75850aec1f4f592b34ce
Chandrahasa Pranava <81606179+rh-cpranava@users.noreply.github.com>
2022-09-28 17:28:10 +0800

    Update check-connections.sh

--- check-connections.sh
+++ check-connections.sh
@@ -1,7 +1,7 @@
 d="$(date +%H%M%S)"
 for i in `oc get pods -n openshift-ingress -oname`
 do
-  oc exec -it $i -- sh -c 'echo "show sess" | socat /var/lib/haproxy/run/haproxy.sock stdio' >> active-connections-$d.txt
+  oc exec -n openshift-ingress -it $i -- sh -c 'echo "show sess" | socat /var/lib/haproxy/run/haproxy.sock stdio' >> active-connections-$d.txt
 done
 cat active-connections-$d.txt | grep srv=p | awk '{print $6}' | uniq -c
 if [ -f active-connections-$d.txt ] ; then
